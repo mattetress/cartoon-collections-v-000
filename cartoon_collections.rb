1347996775486830b7a95407281ea8ab8cc5bc70
Matt Etress <mattetress@me.com>
2018-10-05 03:06:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)# code an argument here
-  array.each { |name| puts name }
+  array.each_with_index { |name, i| puts "#{i + 1}. #{name}" }
 end
 
 def summon_captain_planet# code an argument here
