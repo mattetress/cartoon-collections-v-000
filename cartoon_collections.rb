181553c5445ffbcc45996089ce139310aa62260e
Matt Etress <mattetress@me.com>
2018-10-05 03:14:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,13 @@ def summon_captain_planet(calls)
   calls.collect {|element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    if call.split("").length > 4
+      return true
+    end
+  end
+  false
 end
 
 def find_the_cheese# code an argument here
